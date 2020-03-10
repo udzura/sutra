b9470dcd46c611b999f8688b10dc5d55ba78c2af
Uchio Kondo <udzura@udzura.jp>
2020-03-10 17:53:33 +0900

    Implemented macro dump

--- lib/sutra/macro.rb
+++ lib/sutra/macro.rb
@@ -1,6 +1,56 @@
 require 'yaml'
+require 'optparse'
+
 module Sutra
   class Macro
+    def initialize(argv)
+      @action = argv.shift
+      @options = OptionParser.getopts(argv, 'c:o:', 'category:', 'out:')
+
+      if @action != "dump"
+        raise OptionParser::ParseError
+      end
+    rescue OptionParser::ParseError
+      $stderr.puts usage
+      exit 1
+    end
+
+    def usage
+      <<~USAGE
+        #{File.basename $0} macro
+        Available actions:
+        \tdump [-c|--category CATEGORY_NAME] [-o|--out OUTFILE]
+        \t`- dump current existing macro into YAML
+      USAGE
+    end
+
+    def run
+      case @action
+      when /^d/
+        do_dump
+      else
+        raise "[BUG] unknown subcommand #{ARGV.inspect}"
+      end
+    end
+
+    def do_dump
+      category = @options['category'] || @options['c']
+      yaml = YAML.dump find_all_by_category(category).map(&:to_h)
+
+      out = @options['out'] || @options['o']
+      io = if !out
+             $stdout
+           else
+             File.open(out, 'w')
+           end
+      io.write yaml + "\n"
+      puts "Dump int #{out} successfully" if out
+    end
+
+    def find_all_by_category(category)
+      ms = Sutra::API.current.client.macros(category: category)
+      ms.to_a.map{|m| Entity.new(m) }
+    end
 
     class Entity
       def initialize(data)
@@ -15,7 +65,7 @@ module Sutra
         self["title"]
       end
 
-      def to_managed_h
+      def to_h
         h = @_data.to_hash
         {
           "title" => h["title"],
@@ -28,7 +78,7 @@ module Sutra
       end
 
       def to_yaml
-        YAML.dump to_managed_h
+        YAML.dump to_h
       end
 
       def readable_restriction
